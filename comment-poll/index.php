ad9afe0569e8435f4e83ca538f316b4b32494849
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-04-11 19:39:21 +0700

    Update for Mecha 2.1.x

--- comment-poll/index.php
+++ comment-poll/index.php
@@ -3,10 +3,9 @@
 Asset::set(__DIR__ . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'comment-poll.min.css');
 
 function fn_comment_poll($a, $comment) {
-    $lot = ['path' => str_replace(DS, '/', Path::F($comment->path, LOT))];
-    ob_start();
-    include EXTEND . DS . 'poll' . DS . 'lot' . DS . 'worker' . DS . Plugin::state(__DIR__, 'poll', 'poll/like') . '.php';
-    array_unshift($a, ob_get_clean());
+    $a = ['poll' => Shield::get(Plugin::state('comment-poll', 'poll', 'poll/like'), [
+        'path' => Path::F($comment->path, LOT, '/')
+    ], false)] + $a;
     return $a;
 }
 
